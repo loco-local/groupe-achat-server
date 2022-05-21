9c3940ea62d44b854c1c339c2b7719e63395048f
Vincent Blouin <vincent.blouin@gmail.com>
2022-05-21 16:56:04 -0400

    Buy group order seeder

--- src/seed/seeder.js
+++ src/seed/seeder.js
@@ -1,17 +1,19 @@
 const config = require('../config');
 config.setEnvironment('development');
 const Promise = require('bluebird');
-
+const {addDays} = require("date-fns");
 const {
     sequelize,
     Users,
     Products,
-    BuyGroups
+    BuyGroups,
+    BuyGroupOrders
 } = require('../model')
 
 const users = require('./Users.json')
 const products = require('./Products.json')
 const buyGroups = require('./BuyGroups.json')
+const buyGroupOrders = require('./BuyGroupOrders.json')
 
 module.exports = {
     run: async function () {
@@ -29,6 +31,13 @@ module.exports = {
         await Promise.all(
             products.map(product => {
                 return Products.create(product)
+            })
+        )
+        await Promise.all(
+            buyGroupOrders.map((buyGroupOrder) => {
+                buyGroupOrder.startDate = addDays(new Date(), buyGroupOrder.startDate);
+                buyGroupOrder.endDate = addDays(new Date(), buyGroupOrder.endDate);
+                return BuyGroupOrders.create(buyGroupOrder);
             })
         )
     }
