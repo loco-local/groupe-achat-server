0e8de8cf44e0bf80aa34d60083582c84c4e301be
Vincent Blouin <vincent.blouin@gmail.com>
2023-10-24 14:52:10 -0400

    sequelize version

--- src/model/index.js
+++ src/model/index.js
@@ -2,37 +2,38 @@ const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 const config = require('../config')
+Sequelize.DataTypes.postgres.DECIMAL.parse = parseFloat //  https://github.com/sequelize/sequelize/issues/11855#issuecomment-1382896486
 const db = {}
 const dbConfig = config.get().db
 
 const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.user,
-  dbConfig.password,
-  dbConfig.options
+    dbConfig.database,
+    dbConfig.user,
+    dbConfig.password,
+    dbConfig.options
 )
 
 fs
-  .readdirSync(__dirname)
-  .filter((file) =>
-    file !== 'index.js'
-  )
-  .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file))
-    db[model.name] = model
-  })
+    .readdirSync(__dirname)
+    .filter((file) =>
+        file !== 'index.js'
+    )
+    .forEach((file) => {
+      // for individual model files having `module.exports = (sequelize, DataTypes) => {`
+      const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+      db[model.name] = model
+    })
 
 for (const key in db) {
   const model = db[key]
   if (model.defineAssociationsUsingModels) {
     model.defineAssociationsUsingModels(
-      model,
-      db
+        model,
+        db
     )
   }
 }
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize
-
 module.exports = db
